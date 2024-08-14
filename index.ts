fd252ac6d20fa3cfe87f70a8779880fd9b447c23
ajitdighe <ajitdighe92@gmail.com>
2024-08-14 17:15:44 +0530

    Calculate sum of string

--- index.ts
+++ index.ts
@@ -1,8 +1,37 @@
 const add = (str:String):Number => {
-  // empty string  case 
+  // Empty string  case 
   if (str === "") {
     return 0;
-  }  
+  }
+  
+  // Conver string into array by commas,new line,semicolon,double line separator
+  const strArr = str.split(/[\n,;//]/);
+  
+  let sumStr = 0;
+  let negativeArr = [];
+        
+  // Iterate for loop to calculate sum
+  for (const value of strArr) {
+      if (value.trim() !== '') {
+        const num = parseInt(value.trim(), 10);
+        if (num < 0) {
+          negativeArr.push(num);
+        }
+        sumStr += num;
+      }
+  }
+  
+  // Check negative number is present or not.
+  if (negativeArr.length > 0) {
+    let negativeList = negativeArr.join(",");
+    throw new Error(`Negative numbers not allowed: ${negativeList}`);
+  }
+  return sumStr;
 }
 
-console.log(add(""));
+console.log(add("")); //0
+console.log(add("1")); //1
+console.log(add("1,5"));//6
+console.log(add("1\n2,3")); //6
+console.log(add("//;\n1;2"));//3
+console.log(add("-1,-3,5")); // Negative numbers not allowed: -1,-3
